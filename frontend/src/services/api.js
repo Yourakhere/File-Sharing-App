355a6766db00a726a4aabb9161d1001afe1999f5
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-07-05 22:54:23 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';  
+const API_BASE_URL = 'https://youakher-file-sharing-app.onrender.com';  
 
 export const UploadFile = async (data) => {
   try {
